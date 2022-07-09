0ebdfc8fbb9028e63cec66db7459116a28a862c8
leonel <leonelhcontreras@gmail.com>
2022-07-09 12:02:44 -0400

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae3b2fa830809a1b94fd1160780338e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3e353e6-55e8-49ed-9196-0c7ccdaa8629'
+};
